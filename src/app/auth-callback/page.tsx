1cbb1093d4a18ea35843bdd46f1a54cdb6f90ef1
hfkhrni <hesham.elfakharani@gmail.com>
2023-12-30 21:49:35 +0200

    chore: setup trpc auth callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useRouter, useSearchParams } from 'next/navigation';
 import { trpc } from '../_trpc/client';
 
@@ -7,7 +5,13 @@ function Page() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin');
-  const { data } = trpc.test.useQuery();
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        router.push(origin ? `/${origin}` : '/dashboard');
+      }
+    },
+  });
   return <>{/* <h1>Auth Callback</h1> */}</>;
 }
 
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,15 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, router } from './trpc';
+import { TRPCError } from '@trpc/server';
 
 export const appRouter = router({
-  test: publicProcedure.query(() => 'hello'),
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
+    return { success: true };
+  }),
 });
 
 export type AppRouter = typeof appRouter;
