f17a61328b5628ff775e99e37fdf0d25863b2ebc
hfkhrni <hesham.elfakharani@gmail.com>
2023-12-31 15:37:20 +0200

    feat: open button styles inside upload-button to consumers

--- src/app/_components/upload-button.tsx
+++ src/app/_components/upload-button.tsx
@@ -2,9 +2,18 @@
 
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { cn } from '@/lib/utils';
 import { useState } from 'react';
 
-function UploadButton() {
+function UploadButton({
+  buttonVariant,
+  buttonLabel,
+  buttonClassName,
+}: {
+  buttonLabel?: string;
+  buttonVariant?: 'default' | 'ghost' | 'outline' | 'destructive' | 'secondary';
+  buttonClassName?: string;
+}) {
   const [isOpen, setIsOpen] = useState(false);
   function toggle() {
     setIsOpen(!isOpen);
@@ -18,9 +27,21 @@ function UploadButton() {
         asChild
         onClick={toggle}
       >
-        <Button className="h-8 rounded-sm bg-secondary font-mono text-primary hover:bg-primary hover:text-primary-foreground">
-          Upload
-        </Button>
+        {buttonVariant ? (
+          <Button
+            variant={buttonVariant}
+            className={cn(buttonClassName)}
+          >
+            {buttonLabel ? buttonLabel : 'Upload'}
+          </Button>
+        ) : (
+          <Button
+            variant={buttonVariant}
+            className="h-8 rounded-sm bg-secondary font-mono text-primary hover:bg-primary hover:text-primary-foreground"
+          >
+            {buttonLabel ? buttonLabel : 'Upload'}
+          </Button>
+        )}
       </DialogTrigger>
       <DialogContent className="rounded-none font-mono sm:max-w-[425px]">
         hi
