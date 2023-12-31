bedcc8bd937893026e2f717c6ca2ed1f635c8478
hfkhrni <hesham.elfakharani@gmail.com>
2023-12-31 08:30:12 +0200

    chore: confgure trpc client and auth callback endpoint

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,18 +1,37 @@
+'use client';
+
 import { useRouter, useSearchParams } from 'next/navigation';
 import { trpc } from '../_trpc/client';
-
+import { Loader2 } from 'lucide-react';
+import { useEffect } from 'react';
 function Page() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin');
-  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+  trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
       if (success) {
         router.push(origin ? `/${origin}` : '/dashboard');
       }
     },
+    onError: (err) => {
+      if (err.data?.code === 'FORBIDDEN') {
+        router.push('/');
+      }
+    },
+    retry: true,
+    retryDelay: 500,
   });
-  return <>{/* <h1>Auth Callback</h1> */}</>;
+
+  return (
+    <div className="mt-80 flex w-full flex-col items-center justify-center gap-2">
+      <Loader2 className="mt-6 h-8 w-8 animate-spin  text-zinc-800" />
+      <h3 className="mt-6 font-mono text-xl font-semibold">
+        Setting up your account...
+      </h3>
+      <p className="font-mono">You will be redirected automatically</p>
+    </div>
+  );
 }
 
 export default Page;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,13 +1,29 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
+import { db } from '../db';
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
-    if (!user || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
+    if (!user || !user.email) throw new TRPCError({ code: 'FORBIDDEN' });
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
     return { success: true };
   }),
 });
