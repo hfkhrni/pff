60055959e8d4d02c98fb64ef15242288fec779b6
hfkhrni <hesham.elfakharani@gmail.com>
2024-01-01 21:55:11 +0200

    refactor: organize routes

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, privateProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '../db';
+import { z } from 'zod';
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -35,6 +36,28 @@ export const appRouter = router({
       },
     });
   }),
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
