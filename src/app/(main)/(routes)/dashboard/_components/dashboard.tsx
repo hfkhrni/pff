c1c028d6626dd773c9f795a2676baa6adcbab9b9
hfkhrni <hesham.elfakharani@gmail.com>
2024-01-04 10:11:42 +0200

    fix: unnecessary min-width style was causing initial zoom on mobile

--- src/app/(main)/(routes)/dashboard/_components/dashboard.tsx
+++ src/app/(main)/(routes)/dashboard/_components/dashboard.tsx
@@ -14,7 +14,7 @@ function Dashboard() {
 
   return (
     <>
-      <main className="mx-auto min-w-96 max-w-screen-xl p-4">
+      <main className="mx-auto max-w-screen-xl p-4">
         <div className="mt-8 flex flex-row items-start justify-between gap-4 border-b border-primary pb-5 sm:flex-row sm:items-center sm:gap-0">
           <h1 className="font-mono">FILES</h1>
           <UploadButton />
