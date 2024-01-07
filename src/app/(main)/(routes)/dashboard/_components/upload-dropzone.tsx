57177942ffd5497ae8d0a4911d6c3c9d8c08fa91
hfkhrni <hesham.elfakharani@gmail.com>
2024-01-07 17:39:00 +0200

    feat: upload files to uploadthing and sync to db

--- src/app/(main)/(routes)/dashboard/_components/upload-dropzone.tsx
+++ src/app/(main)/(routes)/dashboard/_components/upload-dropzone.tsx
@@ -7,13 +7,26 @@ import Dropzone from 'react-dropzone';
 import { useToast } from '@/components/ui/use-toast';
 
 import { useUploadThing } from '@/lib/uploadthing';
+import { trpc } from '@/app/(main)/_trpc/client';
+import { useRouter } from 'next/navigation';
 
 function UploadDropzone() {
+  const router = useRouter();
+
   const [isUploading, setIsUploading] = useState<boolean>(false);
   const [progress, setProgress] = useState<number>(0);
 
   const { startUpload } = useUploadThing('pdfUploader');
   const { toast } = useToast();
+
+  const { mutate: startPolling } = trpc.getFile.useMutation({
+    onSuccess: (file) => {
+      setIsUploading(false);
+      router.push(`/dashboard/${file.id}`);
+    },
+    retry: true,
+    retryDelay: 500,
+  });
 
   function simulateProgress() {
     setProgress(0);
@@ -29,40 +42,44 @@ function UploadDropzone() {
 
     return interval;
   }
+
+  async function handleOnDrop(acceptedFile: File[]) {
+    setIsUploading(true);
+    const interval = simulateProgress();
+    const res = await startUpload(acceptedFile);
+
+    if (!res) {
+      toast({
+        title: 'Something went wrong',
+        description: 'Please try again',
+        variant: 'destructive',
+      });
+    }
+
+    const [file] = res ? res : [];
+    const key = file?.key;
+    if (!key) {
+      toast({
+        title: 'Something went wrong',
+        description: 'Please try again',
+        variant: 'destructive',
+      });
+    }
+
+    clearInterval(interval);
+    startPolling({ key });
+    setProgress(100);
+  }
+
   return (
     <Dropzone
       multiple={false}
-      onDrop={async (acceptedFile) => {
-        setIsUploading(true);
-        const interval = simulateProgress();
-        const res = await startUpload(acceptedFile);
-
-        if (!res) {
-          toast({
-            title: 'Something went wrong',
-            description: 'Please try again',
-            variant: 'destructive',
-          });
-        }
-
-        const [file] = res ? res : [];
-        const key = file?.key;
-        if (!key) {
-          toast({
-            title: 'Something went wrong',
-            description: 'Please try again',
-            variant: 'destructive',
-          });
-        }
-        clearInterval(interval);
-        setProgress(100);
-        setIsUploading(false);
-      }}
+      onDrop={handleOnDrop}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <div
           {...getRootProps()}
-          className="mt-6 h-44 rounded-sm border border-dashed border-muted-foreground"
+          className="mt-6 h-48 rounded-sm border border-dashed border-muted-foreground"
         >
           <div
             className="flex h-full w-full items-center justify-center"
@@ -89,23 +106,28 @@ function UploadDropzone() {
                 className="hidden"
               />
 
-              {acceptedFiles ? (
+              {acceptedFiles && acceptedFiles[0] ? (
                 <div className="flex max-w-[calc(100vw-6rem)] items-center divide-x divide-dashed divide-muted-foreground overflow-hidden rounded-sm bg-background outline-dashed outline-[1px] outline-muted-foreground sm:max-w-xs">
                   <div className="grid h-full place-items-center p-2">
                     <File className="h-4 w-4 text-muted-foreground"></File>
                   </div>
-                  <div className="h-full truncate p-2 text-sm text-primary">
+                  <div className="h-full truncate px-2 pt-1.5 text-sm text-primary">
                     {acceptedFiles[0].name}
                   </div>
                 </div>
               ) : null}
-              {isUploading && (
-                <div className="mx-auto mt-4 w-full max-w-xs">
+              {isUploading && progress < 100 && (
+                <div className="mx-auto my-4 w-full max-w-xs">
                   <Progress
                     value={progress}
                     className="h-1 w-full bg-background"
                   />
                 </div>
+              )}
+              {progress === 100 && (
+                <p className="my-4 text-center font-mono text-sm">
+                  REDIRECTING
+                </p>
               )}
             </label>
           </div>
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,10 +1,11 @@
+import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
+  pdfUploader: f({ pdf: { maxFileSize: '16MB' } })
     .middleware(async ({ req }) => {
       const { getUser } = getKindeServerSession();
       const user = await getUser();
@@ -16,6 +17,15 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          userId: metadata.userId,
+          uploadStatus: 'PROCESSING',
+        },
+      });
       return {};
     }),
 } satisfies FileRouter;
