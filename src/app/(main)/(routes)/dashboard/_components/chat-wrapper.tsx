7504733dee46ef03f9226632e0ba93aae512359f
hfkhrni <hesham.elfakharani@gmail.com>
2024-01-17 12:03:41 +0200

    style: height for message box on sm screens

--- src/app/(main)/(routes)/dashboard/_components/chat-wrapper.tsx
+++ src/app/(main)/(routes)/dashboard/_components/chat-wrapper.tsx
@@ -28,7 +28,7 @@ function ChatWrapper({ fileId }: { fileId: string }) {
     );
   if (data?.status === 'PROCESSING')
     return (
-      <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-background rounded-sm bg-secondary p-4">
+      <div className="relative flex h-80 min-h-full flex-col justify-between gap-2 divide-y divide-background rounded-sm bg-secondary p-4">
         <div className="flex flex-1 flex-col items-center justify-center">
           <p className="font-mono">PROCESSING</p>
         </div>
@@ -37,7 +37,7 @@ function ChatWrapper({ fileId }: { fileId: string }) {
     );
   if (data?.status === 'FAILED')
     return (
-      <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-background rounded-sm bg-secondary p-4">
+      <div className="relative flex h-80 min-h-full flex-col justify-between gap-2 divide-y divide-background rounded-sm bg-secondary p-4">
         <div className="flex flex-1 flex-col items-center justify-center">
           <p className="font-mono">FAILED</p>
           <p className="font-mono text-sm text-muted-foreground">
@@ -57,7 +57,7 @@ function ChatWrapper({ fileId }: { fileId: string }) {
       </div>
     );
   return (
-    <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-background/70 rounded-sm bg-secondary p-4">
+    <div className="relative flex h-80 min-h-full flex-col justify-between gap-2 divide-y divide-background/70 rounded-sm bg-secondary p-4">
       <div className="mb-28 flex-1 flex-col justify-between">
         hi
         <Messages />
