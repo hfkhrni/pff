feeed48d5949d18082e07f4625ea702a9552b4a6
hfkhrni <hesham.elfakharani@gmail.com>
2024-01-17 12:02:13 +0200

    feat: upload embeddings to pincone

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,10 @@
 import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { pinecone } from '@/lib/pinecone';
 
 const f = createUploadthing();
 
@@ -26,7 +30,41 @@ export const ourFileRouter = {
           uploadStatus: 'PROCESSING',
         },
       });
-      return {};
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+        console.log(response);
+        const blob = await response.blob();
+        const loader = new PDFLoader(blob);
+        const pages = await loader.load();
+        const noOfPages = pages.length;
+        const pineconeIndex = pinecone.Index('pff');
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+        await PineconeStore.fromDocuments(pages, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+        await db.file.update({
+          data: {
+            uploadStatus: 'SUCCESS',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        await db.file.update({
+          data: {
+            uploadStatus: 'FAILED',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
